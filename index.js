5a0fd75431712fdef1abebe6810d9d4ed78ef4e3
Brarian <b.namonywa@gmail.com>
2019-03-15 23:40:53 -0500

    etsy results except images on dom

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-
-$(document).ready(function () {
-  $('form').submit(function(e){
+$(document).ready(function() {
+  $("form").submit(function(e) {
     e.preventDefault();
     getEtsyResults();
-  })
-})
-
-
-
+  });
+});
 
 function getEtsyResults() {
-  console.log('etsy ran');
+  console.log("etsy ran");
   let term = $("input").val();
-  etsyURL = `https://openapi.etsy.com/v2/listings/active.js?keywords=${term}&limit=12&includes=Images:1&api_key=${esty.KEYSTRING}`
+  etsyURL = `https://openapi.etsy.com/v2/listings/active.js?keywords=${term}&limit=12&includes=Images:1&api_key=${
+    esty.KEYSTRING
+  }`;
 
-  console.log(etsyURL)
+  console.log(etsyURL);
   $.ajax({
-    url: etsyURL, 
-    dataType: 'jsonp', 
-    success: function(data){
-      console.log(data.results)
-    }
-  })
-
+    url: etsyURL,
+    dataType: "jsonp",
+    success: showEtsyResults
+  });
 }
 
-
-
-
-
+function showEtsyResults(data) {
+  data.results.map((result, i) => {
+    console.log()
+    let title  = result.title; 
+    let url = result.url;
+    let description = result.description; 
+    $(".etsy_search_results").append(`<li> ${title, url, description}</li>`);
+  })
+}
