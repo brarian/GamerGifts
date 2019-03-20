b0b14f2886da1be1f19065f80544c3bd8b5295b6
Brarian <b.namonywa@gmail.com>
2019-03-19 19:33:53 -0500

    flex box added to etsy search

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $("form").submit(function(e) {
     e.preventDefault();
     getEtsyResults();
+    
   });
 });
 
@@ -15,17 +16,47 @@ function getEtsyResults() {
   console.log(etsyURL);
   $.ajax({
     url: etsyURL,
+    async: true,
     dataType: "jsonp",
-    success: showEtsyResults
+    beforeSend: function showSpinner() {
+      $(".loader").css("display", "block");
+    },
+    complete: function hideSpinner(){
+      $(".loader").css("display", "none");
+    }, 
+    success: showEtsyResults,
+    error: showError
   });
 }
 
 function showEtsyResults(data) {
+  $(".etsy_search_results").empty();
   data.results.map((result, i) => {
-    console.log()
-    let title  = result.title; 
+    console.log(data.results);
+    let image = result.Images[0].url_170x135; 
+    let title =result.title;
     let url = result.url;
-    let description = result.description; 
-    $(".etsy_search_results").append(`<li> ${title, url, description}</li>`);
-  })
+    let description = result.description.slice(0, 245);
+    // const image_container = 
+    // `<figure >
+    //  <a href=${url} target="_blank"> <img src=${image} alt="${description}"></a>
+    // </figure>`; 
+
+  $(".etsy_search_results").append(
+    `<div class="card" style="min-width: 20rem; max-width: 25rem"> 
+      <img class="card-img-top" src="${image}" alt="card image cap"> 
+        <div class="card-body"> 
+          <h5 class="card-title">${title}</h5>
+          <p class="card-text"> ${description}</p>
+            <a href="${url}" target="_blank" class="btn btn-primary"> See More </a>  
+        </div>
+    </div>`
+    );
+  });
+}
+
+function showError() {
+  $(".etsy_search_results").append(
+    `<h2> There was an error processing your request</h2>`
+  );
 }
